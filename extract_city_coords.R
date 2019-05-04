97fc25b9674513d918b320c715a6da4b2d75f8ce
Aurelie CL <aurelie.c.lafortune@gmail.com>
2019-05-04 10:36:40 -0400

    ajout table et code pour avoir PIB

--- extract_city_coords.R
+++ extract_city_coords.R
@@ -16,8 +16,13 @@ countries <- read.table("Countries2.txt", sep="\t", dec=".", header = FALSE) # i
 
 #garder juste les colonnes nécessaires et les renommer
 countries <- countries[,c(1,5)]
+countries[,2]<- as.character(countries[,2])
+countries[16,2]<- "Aland"
 names(countries)<- c("CC", "country")
 
+gdb <- read.table("GDB.txt", sep="\t", dec=".", header = T)
+names(gdb) <-c ("country", "GDB")
+countries_gdb <- left_join(countries, gdb)
 
 # créer une table test pour les villes avec noms de pays
 
@@ -25,7 +30,9 @@ tab_city <- data.frame(city = c("New York", "Montreal", "Burlington", "London", 
 
 # fusionner les deux tables pour faire une table de référence dans laquelle piger les mots de recherche
 
-tab_ref <- left_join(tab_city, countries)
+tab_ref <- left_join(tab_city, countries_gdb)
+
+
 # Warning message:
 #   Column `country` joining factors with different levels, coercing to character vector 
 # ^^^^^^ message d'erreur normal ^^^^^^
