40943a903a464dc11dee54fcbcbb3e7701d245b3
faresdhane <fs.dhane@gmail.com>
2019-05-04 14:39:48 -0400

    Functions to select data throw specific keywords

---
+++ Select_data.R
@@ -0,0 +1,18 @@
+library(dataone)
+library(XML)
+library(tidyverse)
+
+
+# Getting the ID of the articles
+cn <- CNode("PROD")
+mn <- getMNode(cn, "urn:node:KNB")
+qy_1 <- dataone::query(cn, list(
+  rows = "", 
+  q    = "keywords:Disturbance +OR+ keywords:Fire + OR+  keywords:Forest + OR+  keywords:Vegetation + OR+  keywords:Canopy + OR+  keywords:Structure + OR+  keywords:Demography + OR+  keywords:Fire + OR+   keywords:Management + OR+  keywords:Dynamic + OR+  keywords:Growth+ OR+  keywords:Ecology + OR+  keywords:Inventories+ OR + keywords:Gis", #keyword
+  fq   = "", #filter keyword
+  fl   = "id, checksum, dataUrl, title, keywords"), 
+  as = "data.frame")
+View(qy_1)
+
+my_data <- qy_1 %>% distinct(title, .keep_all = TRUE)
+View(my_data)
